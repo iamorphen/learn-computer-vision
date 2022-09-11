a8943e9b9429c273130878ed4626e0e630ebf84d
orphen <iamorphen@users.noreply.github.com>
2022-09-11 16:58:30 +0900

    feat: enable mathjax and add mkdocs-material-recommended javascript

---
+++ docs/scripts/mathjax.js
@@ -0,0 +1,17 @@
+// TODO(orphen) Replace this with katext (https://katex.org/)?
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise()
+})
